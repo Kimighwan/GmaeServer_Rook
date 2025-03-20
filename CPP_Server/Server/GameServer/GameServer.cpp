bd1000e739ac071de46437a9dd198456ee86fe2c
Kimikhwan <moonfgg@naver.com>
2025-03-20 21:16:17 +0900

    Sleep

--- CPP_Server/Server/GameServer/GameServer.cpp
+++ CPP_Server/Server/GameServer/GameServer.cpp
@@ -1,4 +1,4 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include <iostream>
 
 #include <thread>
@@ -35,6 +35,10 @@ public:
         while (_locked.compare_exchange_strong(expected, desired) == false) 
         {
             expected = false;
+
+            //this_thread::sleep_for(std::chrono::milliseconds(100));
+            this_thread::sleep_for(100ms);
+            //this_thread::yield();
         }
 
 
